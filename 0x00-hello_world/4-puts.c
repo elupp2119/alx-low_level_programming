1973b5ca7d8e9bbbd782300c1b85c1bff8661cd1
eluti <elupp2119@gmail.com>
2022-12-08 13:24:27 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *return: Always 0 (success)
+ *return: Always 0 (Success)
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle" );
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle')
 	return (0);
-	}
-
+}
+	
