99f0bd527585761c58b493e88682d8b64fe40c0e
eluti <elupp2119@gmail.com>
2023-01-17 17:59:08 -0800

    updated 1-init_dog.c

--- 1-init_dog.c
+++ 1-init_dog.c
@@ -6,13 +6,13 @@
  *    * @name: The name of the dog.
  *     * @age: The age of the dog.
  *      * @owner: The owner of the dog.
-         */
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
